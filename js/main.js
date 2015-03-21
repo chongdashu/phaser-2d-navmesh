f2ab8fba85b8f2c8ee0beb1327e2ca9c64478107
= <me@chongdashu.com>
2015-03-21 00:38:31 -0400

    quick and dirty implementation.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,51 @@
  * http://github.com/chongdashu
  */
 
+ (function() {
+    "use strict";
+
+var Set = function() {
+  this.initialize();
+};
+var p = Set.prototype;
+Set.prototype.constructor = Set;
+    
+  p.list = null;
+
+  p.initialize = function() {
+    this.list = [];
+    return this;
+  };
+
+  p.add = function(obj) {
+    if (!this.contains(obj)) {
+      this.list.push(obj);
+    }
+    return this;
+  };
+
+  p.remove = function(obj) {
+    if (this.contains(obj)) {
+      this.list.splice(this.list.indexOf(obj),1);
+    }
+  };
+
+  p.contains = function(obj) {
+    return this.list.indexOf(obj) >= 0;
+  };
+
+  p.isEmpty = function() {
+    return this.list.length === 0;
+  };
+
+  p.toList = function() {
+    return this.list.slice();
+  };
+
+
+window.Set=Set;
+}());
+
 (function() {
     "use strict";
 
@@ -69,7 +114,7 @@ Edge.prototype.constructor = Edge;
 
 
 
-window.Edge=Edge
+window.Edge=Edge;
 }());
 
 
@@ -137,21 +182,31 @@ GameState.prototype.constructor = GameState;
     // @phaser
     p.preload = function() {
         this.game.load.image("panda", "res/panda.png");
-    }
+    };
 
     // @phaser
     p.create = function() {
+        this.createPhysics();
         this.createBitmapContext();
         this.createPolygon();
         this.createPlayer();
+    };
+
+    p.createPhysics = function() {
+      this.game.physics.startSystem(Phaser.Physics.ARCADE);
+      game.world.setBounds(0, 0, 800, 600);
     };
 
     p.createPlayer = function() {
       this.player = this.game.add.sprite(150, 450, "panda");
       this.player.anchor.set(0.5);
       this.player.scale.set(0.1);
+      
+      this.game.physics.arcade.enable(this.player);
 
-    }
+
+
+    };
 
     p.createBitmapContext = function() {
         this.bitmap = this.game.add.bitmapData(this.game.width, this.game.height);
@@ -170,8 +225,8 @@ GameState.prototype.constructor = GameState;
         this.drawPolygon(this.polygon);
 
         for (var i = 0; i < this.triPolies.length; i++) {
-          var poly = this.triPolies[i]
-          this.drawPolygon(poly); 
+          var poly = this.triPolies[i];
+          this.drawPolygon(poly);
         }
 
     };
@@ -200,14 +255,168 @@ GameState.prototype.constructor = GameState;
         this.bitmap.context.fill();
         this.bitmap.context.stroke();  
         this.bitmap.dirty = true;    
-    }
+    };
 
     // @phaser
     p.update = function() {
         // console.log("update");
         this.updateInput();
         this.drawBitmapData();
+        this.updatePathFinding();
     };
+
+    // Path finding
+    // -------------
+
+    p.updatePathFinding = function() {
+      if (this.targetPoint) {
+        var playerTriangle = this.getTriangleFromPoint(this.player.position);
+        var targetTriangle = this.getTriangleFromPoint(this.targetPoint);
+
+        // console.log("playerTriangle=%o", playerTriangle);
+        // console.log("targetTriangle=%o", targetTriangle);
+
+        if (playerTriangle && targetTriangle) {
+          var path = this.astar(playerTriangle, targetTriangle);
+          
+          if (path.length > 0){
+            var first = path[0];
+            var last = path.length - 2 >= 0 ? path[path.length-2] : first;
+
+            console.log("first:%s, last=%s", first.id, last.id);
+            console.log("last=" + last.id);
+            console.log("last.center.x:%s, last.center.y=%s", last.center.x, last.center.y);
+            console.log("this.player.x:%s, this.player.y=%s", this.player.x, this.player.y);
+
+
+            if (Phaser.Math.fuzzyEqual(last.center.x, this.player.x, 1) &&
+                Phaser.Math.fuzzyEqual(last.center.y, this.player.y, 1)){
+                console.log("this.targetPoint=" + this.targetPoint);
+                this.targetPoint = null;
+                this.player.body.velocity.set(0,0);
+
+            }
+          else {
+            this.game.physics.arcade.moveToXY(this.player, last.center.x, last.center.y, 50);
+          }
+            // this.game.physics.arcade.moveToPointer(this.player);
+          }
+
+        }
+
+      
+      }
+    };
+
+    p.getTriangleFromPoint = function(point) {
+      for (var i=0; i < this.triPolies.length; i++) {
+        var triPoly = this.triPolies[i];
+        if (triPoly.contains(point.x, point.y)) {
+          return triPoly;
+        }
+      }
+      return null;
+    };
+
+    p.astar = function (start, end) {
+      
+      var closedSet = new Set();
+      var openSet = new Set().add(start);
+      var from = {};
+
+      var g = {};
+      var f = {};
+
+      g[start] = 0;
+      f[start] = g[start] + this.h(start, end);
+
+      var iters = 0;
+
+      while (!openSet.isEmpty() && iters < 100) {
+        console.log(iters);
+        var current = this.getLowestFNode(openSet, f);
+        if (current == end) {
+          return this.path(from, end);
+        }
+       
+
+        openSet.remove(current);
+        closedSet.add(current);
+
+       
+        for (var i = 0; i < current.neighbors.length; i++) {
+       
+       
+          var neighbor = current.neighbors[i];
+          if (closedSet.contains(neighbor)) {
+            continue;
+          }
+
+          var tempG = g[current] + this.cost(current, neighbor);
+       
+          if (!openSet.contains(neighbor) || tempG < g[neighbor]) {
+       
+            from[neighbor.id] = current;
+            g[neighbor] = tempG;
+            f[neighbor] = g[neighbor] + this.h(neighbor, end);
+            if (!openSet.contains(neighbor)) {
+       
+              openSet.add(neighbor);
+       
+            }
+          }
+      
+        }
+
+        iters++;
+
+      
+      }
+      
+    };
+
+    p.cost = function(a, b) {
+      return Phaser.Point.distance(a.center, b.center) * 14;
+    };
+
+    p.getLowestFNode = function(set, f) {
+      
+      var lowestTriangle = null;
+      var lowestScore = 1000000;
+
+      var triangles = set.toList();
+      for (var i=0; i < triangles.length; i++) {
+        var triangle = triangles[i];
+        var score = f[triangle];
+        if (score < lowestScore) {
+          lowestScore = score;
+          lowestTriangle = triangle;
+        }
+      }
+
+      return lowestTriangle;
+
+    };
+
+    p.path = function(came_from,current) {
+      var c = current;
+      var total_path = [c];
+      
+      while (came_from[c.id]) {
+        c = came_from[c.id];
+        total_path.push(c);
+      }
+    
+      return total_path;
+    };
+
+
+
+    p.h = function(triangle1, triangle2) {
+      return Phaser.Point.distance(triangle1.center, triangle2.center);
+    };
+
+    // -------------
 
     p.cutPolygon = function(polygon) {
       var points = [];
@@ -273,6 +482,8 @@ GameState.prototype.constructor = GameState;
         avgX /= poly.points.length;
         avgY /= poly.points.length;
 
+        poly.center = new Phaser.Point(avgX, avgY);
+
         console.log(avgX, avgY);
 
         var txt = this.game.add.text(avgX, avgY, "#" + this.triPolies.indexOf(poly), { font: "12px Arial", fill: "#ff0044", align: "center" });
@@ -313,11 +524,9 @@ GameState.prototype.constructor = GameState;
           }
         }
       }
-
-
-
-
     };
+
+
 
     p.updateInput = function() {
         var clientX = this.game.input.mousePointer.clientX;
@@ -325,6 +534,13 @@ GameState.prototype.constructor = GameState;
         if (!this.mouseDown && this.game.input.mousePointer.isDown) {
             console.log("down, (%s, %s)", clientX, clientY);
             this.mouseDown = true;
+            this.targetPoint = new Phaser.Point(clientX, clientY);
+
+            var playerTriangle = this.getTriangleFromPoint(this.player.position);
+            var targetTriangle = this.getTriangleFromPoint(this.targetPoint);
+
+            console.log("playerTriangle=%o, index=%s", playerTriangle, playerTriangle ? playerTriangle.id : "");
+            console.log("targetTriangle=%o, index=%s", targetTriangle, targetTriangle ? targetTriangle.id : "");
         }
         else if (this.mouseDown && !this.game.input.mousePointer.isDown) {
             this.mouseDown = false;
@@ -344,7 +560,7 @@ GameState.prototype.constructor = GameState;
         
     };
 
-window.GameState=GameState
+window.GameState=GameState;
 }());
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, "main");
